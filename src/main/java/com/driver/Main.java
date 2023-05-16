c2b3b1adb1ea3fed71be7d8c5aff0b22130992ba
Areeff <p.areeff786@gmail.com>
2023-05-16 17:53:00 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,7 +8,7 @@ public class Main {
   }
   public static class B extends A{
       public  String meth(){
-          return "Method is overridden in Extended class B";
+          return "Method is overrridden in Extended class B";
       }
   }
   public static void main(String [] args){
